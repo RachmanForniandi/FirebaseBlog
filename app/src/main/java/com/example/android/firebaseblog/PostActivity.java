2e677bd0cda5162c54804559b6e0bf99953dac28
RachmanForniandi <rachmanforniandi@gmail.com>
2017-04-18 01:45:47 +0700

    menampilkan data di database nya firebase

--- app/src/main/java/com/example/android/firebaseblog/PostActivity.java
+++ app/src/main/java/com/example/android/firebaseblog/PostActivity.java
@@ -12,6 +12,8 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -25,9 +27,10 @@ public class PostActivity extends AppCompatActivity {
 
     private Uri mImageUri = null;
 
-    private static final int GALLERY_REQUEST=1;
+    private static final int GALLERY_REQUEST = 1;
 
     private StorageReference mStorage;
+    private DatabaseReference mDatabase;
 
     private ProgressDialog mProgress;
 
@@ -37,6 +40,7 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post);
 
         mStorage = FirebaseStorage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
 
         mSelectImage =(ImageButton)findViewById(R.id.imageSelect);
         mPostTitle =(EditText)findViewById(R.id.titleField);
@@ -49,6 +53,7 @@ public class PostActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
+
                 Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 galleryIntent.setType("image/*");
                 startActivityForResult(galleryIntent,GALLERY_REQUEST);
@@ -69,18 +74,26 @@ public class PostActivity extends AppCompatActivity {
         mProgress.setMessage("Posting to Blog ...");
         mProgress.show();
 
-        String title_val = mPostTitle.getText().toString().trim();
-        String desc_val = mPostDesc.getText().toString().trim();
+        final String title_val = mPostTitle.getText().toString().trim();
+        final String desc_val = mPostDesc.getText().toString().trim();
 
         if(!TextUtils.isEmpty(title_val)&& !TextUtils.isEmpty(desc_val)&& mImageUri !=null){
-            StorageReference filepath = mStorage.child("Blog_Images").child(mImageUri.getLastPathSegment());
 
+            StorageReference filepath = mStorage.child("Blog_Images").child(mImageUri.getLastPathSegment());
             filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    DatabaseReference newPost = mDatabase.push();
+
+                    newPost.child("title").setValue(title_val);
+                    newPost.child("desc").setValue(desc_val);
+                    newPost.child("image").setValue(downloadUrl.toString());
+
                     mProgress.dismiss();
+
+                    startActivity(new Intent(PostActivity.this, MainActivity.class));
                 }
             });
 
